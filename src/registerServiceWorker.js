cf858bb7ff2fb9462426a53687c7bee9730aaa38
Alexandre Soares <ale_santos.soares@hotmail.com>
2019-10-09 18:15:57 -0300

    add register to service worker (#6)

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -111,7 +111,7 @@ function checkValidServiceWorker(swUrl) {
 export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
-      registration.unregister();
+      registration.register();
     });
   }
 }
