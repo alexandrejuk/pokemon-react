af81f744e6b5b02e06bfbed091f77697b17cff24
Alexandre Soares <alexandre@alexandre.soares>
2023-11-30 16:33:17 -0300

    update

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -5,8 +5,9 @@ import loadPokeball from '../../assets/imgs/load-pokeball.gif'
 
 class Loading extends Component {
   render() { 
+    
     return (
-      <div className="pokeball-load">
+      <div className="pokeball-load" >
         <img src={loadPokeball} alt="loading..." />   
         <h1>Loading...</h1>  
       </div>
